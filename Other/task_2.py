72e7e3fe824d28072aadd5760186f62a1bcfd5c4
finger_to_the_sky <mzagrebam@gmail.com>
2022-06-19 15:26:51 +0300

    task_2

--- Other/task_2.py
+++ Other/task_2.py
@@ -4,9 +4,10 @@ class Solution:
     def __init__(self, ls_1, ls_2):
         self.ls_1 = ls_1
         self.ls_2 = ls_2
-    def MedianFind(self):
-        result = sorted(self.ls_1 + self.ls_2)
 
+    @staticmethod
+    def MedianFind(list_1, list_2):
+        result = sorted(list_1 + list_2)
         l = len(result)
         print(result)
 
@@ -18,10 +19,7 @@ class Solution:
             return (result[l // 2] + result[(l // 2) + 1]) / 2
 
 
+
 list_1 = [1, 2, 5, 7, 8]
 list_2 = [1, 4, 5]
-
-s = Solution(list_1, list_2)
-print(s.MedianFind())
-
-
+print(Solution.MedianFind(list_1, list_2))
